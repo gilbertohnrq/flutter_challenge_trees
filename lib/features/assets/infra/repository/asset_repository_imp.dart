b3b8cf4e27ff0b80fadbd7de9259fdec3f41a6ff
Gilberto <gilbertohnrq@gmail.com>
2025-06-30 15:56:11 -0300

    feat: implement asset repository

---
+++ lib/features/assets/infra/repository/asset_repository_imp.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:result_dart/result_dart.dart';
+import 'package:tractian_test/features/assets/datasource/i_asset_service.dart';
+import 'package:tractian_test/features/assets/domain/repository/i_asset_repository.dart';
+import 'package:tractian_test/features/assets/infra/models/response/get_assets_response.dart';
+
+class AssetsRepository extends IAssetRepository {
+  final IAssetService _service;
+
+  AssetsRepository({required IAssetService service})
+      : _service = service;
+
+  @override
+  AsyncResult<GetAssetsResponse, DioException> get(id) async {
+    try {
+      final result = await _service.get(id);
+      return Success(result);
+    } catch (e) {
+      return Failure(e as DioException);
+    }
+  }
+}
